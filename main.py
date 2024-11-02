4503b740f5229fb8a3fbb78804eab535513222d9
dnxa <kuridze0@gmail.com>
2024-11-02 11:54:02 +0400

    Add divide and multiply functions.

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+def div(a, b):
+    return a / b
+
+def mult(a, b):
+    return a * b
+
 print(add(5, 10))
 print(sub(10, 5))
+print(div(10, 2))
+print(mult(10, 5))
